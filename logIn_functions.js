95790182e45e27b80b3e71e7057192cad8d4e6a4
rollegocoaster <49416717+rollegocoaster@users.noreply.github.com>
2020-03-18 21:11:33 -0600

    updated dropdowns and added toggle feature
    when logged_in is true the profile photo activates a dropdown and now when log in is pressed and the form isn't empty it toggles the navbar to have the logged in elements and if the log out button is pressed the nav bar shows the logged out elements

--- logIn_functions.js
+++ logIn_functions.js
@@ -1,27 +1,33 @@
 var testcolors = ["#FF750F","#E84B0E","#FF381B","#E80E17"]
 
 
-function loadifSignedIn(condition)//set condition to 'true' for profile button 'false' for sign in and log in
+function loadifSignedIn(logged_in)//set condition to 'true' for profile button 'false' for sign in and log in
 {
   console.log("test");
+  var replace = document.getElementById("login-elements");
+  if(replace!=null){
+    replace.remove();
+  }
   var nav = document.getElementById("navigation");//make sure to rename the navbar to navigation
   var navChildren = nav.childNodes;
   navChildren["5"].setAttribute("id","navbarNavDropdown");
    nav.style.backgroundColor ="#E84B0E";
    nav.setAttribute("class","navbar navbar-expand-lg navbar-dark");
-   if(condition=="false"){
-     nav.innerHTML += "<button type=\"button\" class=\"btn btn-outline-light\" data-toggle=\"modal\" data-target=\"#signup\"> Sign up </button>"
-     +"<div class=\"dropdown\" id=\"log_in_dropdown\"><button type=\"button\" class=\"btn btn-outline-light dropdown-toggle\" data-toggle=\"dropdown\">log in</button>"
-     +"<div class=\"dropdown-menu dropdown-menu-right\" style=\"background-color=rgb(30,201,110)\">"
-     +" <form><div class=\"form-group\" style=\"background-color=rgb(30,201,110)\">"
+   nav.innerHTML += "<div id='login-elements'>";
+   nav = document.getElementById('login-elements');
+   if(logged_in=="false"){
+     nav.innerHTML += "<div class=\"dropdown\" id=\"log_in_dropdown\"><button type=\"button\" class=\"btn btn-outline-light dropdown-toggle\" data-toggle=\"dropdown\">log in</button>"
+     +"<div class=\"dropdown-menu dropdown-menu-right px-1\" style=\"background-color:#E84B0E;color:white\">"
+     +" <form><div class=\"form-group\">"
      +"     <label for=\"Log-in-Username\">User Name: </label>"
      +"                 <input type=\"text\" class=\"form-control\" id=\"log-in-Username\" placeholder=\"Name\""
      +"                   required=\"required\">"
      +"  </div><div class=\"form-group\">"
      +"                   <label for=\"log-in-password\">Password:</label>"
      +"                   <input type=\"password\" class=\"form-control\" id=\"log-in-password\" placeholder=\"password\" required=\"required\" onclick=\"\">"
-     +"<button type=\"submit\" class=\"form-control btn-outline-dark\" style=\"backgroundColor:rgb(30,201,110)\" onclick=\"log_in()\">log in </button>"
-     +"</form></div></div></div>"
+     +"</div><div style='margin:auto'><button type=\"submit\" class=\"btn btn-outline-light\" style=\"backgroundColor:rgb(30,201,110)\" onclick=\"logIn()\">log in </button></div>"
+     +"</form><div class='dropdown-divider'></div><p> New here? </p<>"
+     +"<button type=\"button\" class=\"btn btn-outline-light\" data-toggle=\"modal\" data-target=\"#signup\"> Sign up </button></div></div>"
      +"<div class=\"modal fade\" id=\"signup\" tabindex=\"-1\" role=\"dialog\" aria-labelledby=\"exampleModalCenterTitle\" aria-hidden=\"true\">"
      + "       <div class=\"modal-dialog modal-dialog-centered\" role=\"document\">"
      +"           <div class=\"modal-content\">"
@@ -49,8 +55,15 @@ function loadifSignedIn(condition)//set condition to 'true' for profile button '
      +"             </div></div>"
      +"             <div class=\"modal-footer\">"
      +"               <button type=\"button\" class=\"btn btn-secondary\" onclick=\"verify()\">sign up</button> </div></div></div>";
-   } else if(condition=="true"){
-      nav.innerHTML += "<img src=\"https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png\" class=\"rounded-circle\" id=\"profile_photo\" width=\"50\" onclick=\"alert(\"wow\");\" height=\"50\" style=\"margin:0px 20px\">"
+   } else if(logged_in=="true"){
+      nav.innerHTML += "<div class=\"dropdown\" id=\"log_in_dropdown\">"
+       + "<img src=\"https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png\" class=\"rounded-circle\" id=\"profile_photo\" width=\"50\" data-toggle=\"dropdown\" height=\"50\" style=\"margin:0px 20px\">"
+       + "<div class=\"dropdown-menu dropdown-menu-right px-1\" style=\"background-color:#E84B0E;color:white\">"
+       + "<b class=\"dropdown-header font-weight-bold\" style='color:#cccccc' id=\"navbar_username_text\"> <img src=\"https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png\" id=\"profile_photo_small\" width=\"25\" height=\"25\">  default username</b>" //replace text with username from database
+
+       + "<a class='nav-link' style='color:#eeeeee' href="/*link to profile settings page*/ + "#" + " id='profile_settings_link'> profile settings </a>"
+       + "<div class='dropdown-divider'></div> <button type=\"button\" class=\"btn btn-outline-light\" id=\"log_Out_Button\" onclick=\"logOut()\"> log out </button> "
+       + "</div>";
       // drop down with profile settings and log out
 
    }
@@ -109,12 +122,14 @@ function verify(){
 }
 
 
-function log_in(){
+function logIn(){
   var uName = document.getElementById("log-in-Username");
   var password = document.getElementById("log-in-password");
-  var test = document.getElementById("log_in_dropdown")
+  var test = document.getElementById("log_in_dropdown");
+  var notEmpty = true;
   if(uName.value==""){
     uName.style.borderColor="red";
+    notEmpty = false;
   //  setTimeout(() => {$('#log_in_dropdown').dropdown('toggle');
 //                      test.setAttribute("class", "dropdown show")
   //                    test.childNodes[1].setAttribute("aria-expanded",'true')}, 100);
@@ -124,5 +139,20 @@ function log_in(){
   }
    if(password.value==""){
     password.style.borderColor="red";
+    notEmpty=false;
   }
+
+  if(notEmpty){
+    // reload the navbar with username credentials
+
+    // ^^ here
+    loadifSignedIn('true');
+  }
+
+}
+
+function logOut(){
+  loadifSignedIn('false');
+
+
 }
