081ca509b398b59547d9be14a488598541ba7c92
Coren Lam <49416702+corenlam8@users.noreply.github.com>
2020-04-30 19:04:34 -0600

    restaurant information

---
+++ RestaurantInformation/restaurantpage.js
@@ -0,0 +1,35 @@
+app.get('/restaurant_info/post', function(req,res) {
+  var restaurant_id= select restaurantName from restaurant ;
+  var location = select address  from restaurant ;
+  var reviews = select comment from review;
+  var phonenumber= select phonenumber from restaurant ;
+  db.task('get-everything', task=>{
+    return task.batch ([
+      task.any(restaurant_id),
+      task.any(location),
+      task.any(review),
+      task.any(phonenumber)
+    ]);
+  })
+  .then(data=> {
+    res.render('pages/restaurant_info', {
+      my_title:"Restaurant information",
+      data: data[0],
+      restaurant: restaurant_id,
+      Location: location,
+      Reviews: review,
+      Phonenumbers: phonenumber,
+    })
+  })
+  .catch(err=> {
+    console.log('error',err);
+    res.render('pages/restaurant_info/select_restaurant', {
+      title:"Restaurant information",
+      data: '',
+      restaurant:'',
+      Location: '',
+      Reviews: '',
+      Phonenumbers: '',
+    })
+  });
+});
